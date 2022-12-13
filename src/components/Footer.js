55056cbb633e07e8125da2b56286db4e727b7456
Jelou <arjelou.jelou>
2022-12-14 06:34:38 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,45 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
+   
     return (
         <>
-        <div>
-            <div className="container footer">
-                <div className="d-inline-block">
-                    <button className="btn btn-primary m-1">FAQs</button>
-                    <button className="btn btn-primary m-1">Socials</button>
-                    <button className="btn btn-primary m-1">Track my Parcel</button>
-                    <button className="btn btn-primary m-1">Socials</button>           
+            <div className="container mb-2footer">
+                <div className="row justify-content-md-between mt-3">
+                    <div className="col">
+                        <h2>ACMADE</h2>
+                    </div>
+                    <div className="col">
+                        
+                            <div className="list-category">
+                               <ul>
+                                <h5>Category</h5>
+                                <li><a href=".">Dress</a></li>
+                                <li><a href=".">Knitwear</a></li>
+                                <li> <a href=".">Crop Tops</a></li>
+                                <li> <a href=".">Pantsuit divlection</a></li> 
+                                <li> <a href=".">Sharp Sweet Menswear</a></li> 
+                                <li> <a href=".">Adventures Accessories</a></li> 
+                               </ul>
+                            </div>
+                    </div>
+                    <div className="col">
+                        <h6>Social</h6>
+                        <ul>
+                            <li><a href=".">Facebook</a></li>
+                            <li><a href=".">Instagram</a></li>
+                            <li> <a href=".">YouTube</a></li> 
+                        </ul>
+                    </div>
+                </div>
+                <div className="container">
+                <div className="row">
+                    <div className="col text-center mt-3 mb-3">
+                     <span className="text-center">Copyright 2022 by ACMADE Data. All Rights Reserved.</span>
+                    </div>
                 </div>
             </div>
-            <div className="text-center">
-                <span className="text-center">Copyright 2022 by ACMADE Data. All Rights Reserved.</span>
             </div>
-        </div>
         </>
     );
   }
