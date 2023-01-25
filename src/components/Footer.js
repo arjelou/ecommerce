803c676498977c36c280f078679752c13e44637b
Jelou <arjelou.jelou@gmail.com>
2023-01-25 11:03:47 +0800

    col

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { BsFacebook,BsInstagram,BsYoutube } from 'react-icons/bs';
 export default class Footer extends React.Component {
   render() {
@@ -11,32 +12,47 @@ export default class Footer extends React.Component {
                         <h2>ACMADE</h2>
                     </div>
                     <div className="col">
-                            <div className="list-category">
-                               <ul>
-                                <h5>Category</h5>
-                                <li><a href=".">Dress</a></li>
-                                <li><a href=".">Knitwear</a></li>
-                                <li> <a href=".">Crop Tops</a></li>
-                                <li> <a href=".">Pantsuit divlection</a></li> 
-                                <li> <a href=".">Sharp Sweet Menswear</a></li> 
-                                <li> <a href=".">Adventures Accessories</a></li> 
-                               </ul>
-                            </div>
+                        <div className="list-category">
+                            <ul>
+                            <h5>Design</h5>
+                            <li><a href=".">Dress</a></li>
+                            <li><a href=".">Knitwear</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="col d-flex gap-3">
-                        <h5>Social</h5>
-                        <a href="."><BsFacebook size={30}/></a>
-                           <a href="."><BsInstagram size={30}/></a>
-                             <a href="."><BsYoutube size={30}/></a>
+                    <div className="col">
+                        <div className="list-category">
+                            <ul>
+                            <h5>Company</h5>
+                                <li><a href=".">About Us</a></li>
+                                <li><a href=".">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="list-category">
+                            <ul className="">
+                                <h5>Social</h5>
+                                <div className="d-flex gap-3">
+                                    <a href="."><BsFacebook size={25} color="blue"/></a>
+                                    <a href="."><BsInstagram size={25} color="orange"/></a>
+                                    <a href="."><BsYoutube size={30} color="red"/></a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="container">
-                <div className="row">
-                    <div className="col text-center mt-3 mb-3">
-                     <span className="text-center">Copyright 2022 by ACMADE Data. All Rights Reserved.</span>
+                <div className="container-fluid mt-5">
+                    <div className="row d-flex justify-content-md-between">
+                        <div className="col">
+                            <NavLink className="">Copyright 2022 by ACMADE Data. All Rights Reserved.</NavLink>   
+                        </div>
+                        <div className="col d-flex gap-4">   
+                            <NavLink to="">Privacy Policy</NavLink>
+                            <NavLink to="">Term & Condition</NavLink>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </>
     );
