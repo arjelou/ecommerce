01c673e03fd0ad4356c0532253d6810a7d1dc47a
Jelou <arjelou.jelou@gmail.com>
2023-02-10 14:22:59 +0800

    clean up

--- src/components/mainNavbar.js
+++ src/components/mainNavbar.js
@@ -17,15 +17,12 @@ export default class mainNavbar extends React.Component  {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-div container-fluid">
         <div className="container">
-          <a className="navbar-brand" href=".">ACMADE MAGSIGE</a>
+          <a className="navbar-brand" href=".">ACMADE MAGSIGE Main</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="offcanvas offcanvas-end" tabIndex="1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <ul className="navbar-nav justify-content-start">
-            {/* <li className="nav-item unactive-category-btn">
-                <a className="nav-link active" aria-current="page" href=".">Dashboard</a>
-              </li> */}
               <li className="nav-item dropdown unactive-category-btn">
                 <a className="nav-link dropdown-toggle" href="." role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Company
@@ -34,7 +31,6 @@ export default class mainNavbar extends React.Component  {
                   <NavLink className={({isActive}) => isActive ? "active_category_dropdown" : "unactive_category_dropdown"} to='/dashboard/listofcustomers'>About Us</NavLink>
                   <NavLink className="unactive_category_dropdown" data-bs-toggle="modal" data-bs-target="#addNewModalCreateCustomer">Contact Us</NavLink>
                   <NavLink className="unactive_category_dropdown" data-bs-toggle="modal" data-bs-target="#addNewModalCreateCustomer">Join Our Team</NavLink>
-
                 </ul>
               </li>
               <li className="nav-item dropdown unactive-category-btn">
@@ -45,8 +41,6 @@ export default class mainNavbar extends React.Component  {
                   <NavLink className={({isActive}) => isActive ? "active_category_dropdown" : "unactive_category_dropdown"} to='/alldesign'>All Design</NavLink>
                   <NavLink className={({isActive}) => isActive ? "active_category_dropdown" : "unactive_category_dropdown"} to='/asasa'>Uniforms</NavLink>
                   <NavLink className={({isActive}) => isActive ? "active_category_dropdown" : "unactive_category_dropdown"} to='/asas'>Gre Brand</NavLink>
-
-
                 </ul>
               </li>
               <li className="nav-item dropdown unactive-category-btn">
@@ -58,10 +52,6 @@ export default class mainNavbar extends React.Component  {
                   <NavLink className={({isActive}) => isActive ? "active_category_dropdown" : "unactive_category_dropdown"} to='/dashboard/categories'>Agriculture</NavLink>
                 </ul>
               </li>
-              {/* <li className="list-group-item d-flex align-items-start unactive-category-btn">
-                <a className="nav-link me-auto" aria-current="page" href=".">Notification</a>
-                <span className="badge bg-primary rounded-pill">2</span>
-              </li> */}
             </ul>
           </div>
           <div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -87,57 +87,5 @@ return (
   )
 }
 
-
 export default Login; 
 
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-
-// export default class Login extends React.Component {
-// constructor(props) {
-//    super(props);
-//    this.state = {
-//     dat: [],
-//    }
-   
-// }
-
-
-    
-//   render(){
-//     return (
-//         <>
-//             <div className="container">
-//                 <div className='row justify-content-md-center mb-5'>
-//                     <div className='col-lg-4 col-md-auto login'>
-//                         <h2 className='text-center mb-4'><strong>Login to your account</strong></h2>
-//                         <div className='container'>
-//                             <form className='col'>
-//                                 <label for='email'>Email or Username</label>
-//                                 <input type='email' id='email' placeholder='john@example.com' className='inpuInactive'/>
-//                                 <label for='password'>Password</label>
-//                                 <input type='password' id='password' placeholder='*********' className='inpuInactive'/>
-//                                 <button className='btnDefault'>Continue</button>
-//                                 <span>Don't have an account? <a href='.' className='text-decoration-none'><NavLink to='/signup'><strong>Sign up</strong></NavLink></a></span>
-//                                 <hr />
-//                                 <button className='loginOR mb-2'>Continue with Google</button>
-//                                 <button className='loginOR'>Continue with Facebook</button>
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//       )
-//   }
-// }
