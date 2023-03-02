7133e62361f3fd3f3fd0ac8a734039c79055a7c7
Jelou <arjelou.jelou@gmail.com>
2023-03-02 16:04:15 +0800

    add contact form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,11 +13,45 @@ return (
             contact you promptly. And while you're at it, signup for our newsletter to stay informed on all the 
             latest services and products, research and awesome designs that we've captured along the way.
             </p>
-            <form>
-                <input type="email" name="get_in_touch" />
-                <button type="submit" htmlFor="get_in_touch">Subscribe</button>
-            </form>
         </article>
+        <div className="contact_form">
+            <form>
+                <label htmlFor="your_name">Your name</label>
+                <input type="text" name="your_name" />
+                <label htmlFor="your_phone">Phone</label>
+                <input type="number" name="your_phone" />
+                <label htmlFor="your_email">Email</label>
+                <input type="email" name="your_email" />
+                <div className="preferred_method">
+                    <p>Preferred method of contact:</p>
+                    <label>
+                        <input type="radio" name="email"/>
+                        <span></span>
+                        Email
+                    </label>
+                    <label>
+                        <input type="radio" name="phone" />
+                        <span></span>
+                        Phone
+                    </label>
+                    <label>
+                        <input type="radio" name="message_text" />
+                        <span></span>
+                        Text
+                    </label>
+                </div>
+                <label htmlFor="text">Your message/Question</label>
+                <textarea name="message"/>
+                <div className="signup_newsletter">
+                    <label>
+                        <input type="checkbox" name="" />
+                        <span></span>
+                        Sign up our newsletter!
+                    </label>
+                </div>
+                <button type="submit">Send Request</button>
+            </form>
+        </div>
     </section>
     <section className="container-fluid social_footer">
         <article>
@@ -39,7 +73,6 @@ return (
             <p>Address: Calinan, Davao City, 8000</p>
     </article>
 </div>
-
 <div className="container-fluid copyright_footer">
     <span>ACMADE MAGSIGE@2023</span>
 </div>
